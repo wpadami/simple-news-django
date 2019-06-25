dd11666820ac1f987129f32a7683714ac716b12a
Serkan Algur <info@wpadami.com>
2019-06-26 00:36:45 +0400

    views additions

--- news/views.py
+++ news/views.py
@@ -22,6 +22,7 @@ class DetailedView(generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
         return context
 
 class CategoryListView(generic.ListView):
@@ -32,6 +33,7 @@ class CategoryListView(generic.ListView):
         category  = get_object_or_404(Category, cat_slug=self.kwargs.get('cat_slug'))
         context = super().get_context_data(**kwargs)
         context.update({
-            'news_list': Post.objects.order_by('-created_on').filter(category_id=category.id),
+            'news_list': News.objects.order_by('-created_on').filter(category_id=category.id),
+            'cat' : category,
         })
         return context
