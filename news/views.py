5ffc1d96ee0f1232c999b4ed94eee7aae81ba770
Serkan Algur <info@wpadami.com>
2019-06-25 14:31:15 +0400

    Simple Templating and Views

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-rom django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
