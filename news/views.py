1e3f28f9e4c49ca9ebda908e19125e0a5fd3e63b
Serkan Algur <info@wpadami.com>
2019-06-28 13:41:39 +0400

    Changes for category index

--- news/views.py
+++ news/views.py
@@ -14,6 +14,9 @@ class BlogIndexView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['now'] = timezone.now()
+        context.update({
+            'news_list': News.objects.order_by('-created_on')
+        })
         return context
 
 class DetailedView(generic.DetailView):
